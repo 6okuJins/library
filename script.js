307fa0028ec2043041e9201e051121e031b9353b
6okuJins <d.ma@mail.utoronto.ca>
2022-05-03 15:27:46 -0400

    Refactor function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const libraryContainer = document.querySelector(".library-container");
 const addButton = document.querySelector(".library-container .add-book")
 let myLibrary = [];
 
-addButton.addEventListener("click", addToLibrary);
+addButton.addEventListener("click", addToArray);
 
 function Book(title, author, pages, complete) {
     this.title = title;
@@ -15,7 +15,7 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.complete ? "completed" : "not yet read")}.`
 }
 
-function addToLibrary() {
+function addToArray() {
     const title = prompt("Please enter the title:");
     const author = prompt("Please enter the author:");
     const pages = prompt("Please enter the number of pages:")
